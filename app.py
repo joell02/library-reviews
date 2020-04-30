f570f86ebd41f6fd93daa3c655e4369d9c31e610
Joell Sebastiampillai <joelljps@hotmail.com>
2020-04-30 01:31:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
     return render_template("startscreen.html", isLoggedIn=False)
 
